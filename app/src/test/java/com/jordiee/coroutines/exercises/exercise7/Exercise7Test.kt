51066ea9e32354eb114334fedead5d28ada87b92
vaibhav <vaibhavg7005@gmail.com>
2024-01-18 01:55:02 +0530

    Coroutine mechanics details

--- app/src/test/java/com/jordiee/coroutines/exercises/exercise7/Exercise7Test.kt
+++ app/src/test/java/com/jordiee/coroutines/exercises/exercise7/Exercise7Test.kt
@@ -20,9 +20,36 @@ class Exercise7Test {
         runBlocking {
             val scopeJob = Job()
             val scope = CoroutineScope(scopeJob + CoroutineName("outer scope") + Dispatchers.IO)
-
-
-            scopeJob.join()
+            val mainJob = scope.launch {
+                try {
+                    delay(100)
+                    withContext(CoroutineName("withFirstContext")) {
+                        try {
+                            delay(100)
+                            withContext(CoroutineName("withSecondContext")) {
+                                try {
+                                    printJobsHierarchy(scopeJob)
+                                    delay(100)
+                                    println("second context completed")
+                                } catch (exception: CancellationException) {
+                                    println("second context haulted")
+                                }
+                            }
+                            println("first context completed")
+                        } catch (exception: CancellationException) {
+                            println("first context haulted")
+                        }
+                    }
+                    println("outer context executed")
+                } catch (exception: CancellationException) {
+                    println("outer context haulted")
+                }
+            }
+            scope.launch {
+                delay(250)
+                scope.cancel()
+            }
+            mainJob.join()
             println("test done")
         }
     }
@@ -36,9 +63,37 @@ class Exercise7Test {
         runBlocking {
             val scopeJob = Job()
             val scope = CoroutineScope(scopeJob + CoroutineName("outer scope") + Dispatchers.IO)
-
-
-            scopeJob.join()
+           val job = scope.launch {
+                try {
+                    delay(100)
+                    withContext(CoroutineName("InnerCoroutine") + Dispatchers.IO) {
+                        try {
+                            delay(100)
+                            launch(CoroutineName("inner new coroutine")) {
+                                try {
+                                    printJobsHierarchy(scopeJob)
+                                    delay(100)
+//                        scope.cancel()
+                                    println("inner new coroutine executed")
+                                } catch (ex: CancellationException) {
+                                    println("inner new coroutine dropped")
+                                }
+                            }
+                            println("inner coroutine executed")
+                        } catch (ex: CancellationException) {
+                            println("inner coroutine haulted")
+                        }
+                    }
+                    println("outer coroutine executed")
+                } catch (ex: CancellationException) {
+                    println("outer coroutine dropped")
+                }
+            }
+            scope.launch {
+                delay(250)
+                scope.cancel()
+            }
+            job.join()
             println("test done")
         }
     }
@@ -52,9 +107,36 @@ class Exercise7Test {
         runBlocking {
             val scopeJob = Job()
             val scope = CoroutineScope(scopeJob + CoroutineName("outer scope") + Dispatchers.IO)
-
-
-            scopeJob.join()
+            val job = scope.launch {
+                try {
+                    delay(100)
+                    withContext(CoroutineName("withContext") + Dispatchers.IO) {
+                        try {
+                            delay(100)
+                            scope.launch(CoroutineName("innerCoroutine")) {
+                                try {
+                                    printJobsHierarchy(scopeJob)
+                                    delay(100)
+                                    println("inner coroutine executed")
+                                } catch (ex : CancellationException) {
+                                    println("inner coroutine haulted")
+                                }
+                            }
+                            println("with context completed")
+                        } catch (ex: CancellationException) {
+                            println("with context haulted")
+                        }
+                    }
+                    println("outer coroutine executed")
+                } catch (ex: CancellationException) {
+                    println("outer coroutine haulted")
+                }
+            }
+            scope.launch {
+                delay(250)
+                scope.cancel()
+            }
+            job.join()
             println("test done")
         }
     }
